6f21bdf44d4bc17a7052e648d2a0476bd8183b63
GBoshnakov <boshnakov.g@gmail.com>
2022-04-13 19:53:22 +0300

    new components added

--- src/app/advert/advert-routing.module.ts
+++ src/app/advert/advert-routing.module.ts
@@ -1,10 +1,25 @@
 import { RouterModule, Routes } from "@angular/router";
+import { AdvertDetailsComponent } from "./advert-details/advert-details.component";
 import { AdvertsPageComponent } from "./adverts-page/adverts-page.component";
+import { CreateNewComponent } from "./create-new/create-new.component";
+import { MyAdvertsComponent } from "./my-adverts/my-adverts.component";
 
 const routes: Routes = [
     {
         path: 'adverts',
         component: AdvertsPageComponent
+    },
+    {
+        path: 'create',
+        component: CreateNewComponent
+    },
+    {
+        path: 'details',
+        component: AdvertDetailsComponent
+    },
+    {
+        path: 'my',
+        component: MyAdvertsComponent
     }
 ]
 
--- src/app/advert/advert.module.ts
+++ src/app/advert/advert.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdvertsPageComponent } from './adverts-page/adverts-page.component';
 import { AdvertRoutingModule } from './advert-routing.module';
+import { CreateNewComponent } from './create-new/create-new.component';
+import { MyAdvertsComponent } from './my-adverts/my-adverts.component';
+import { AdvertDetailsComponent } from './advert-details/advert-details.component';
 
 
 
 @NgModule({
   declarations: [
-    AdvertsPageComponent
+    AdvertsPageComponent,
+    CreateNewComponent,
+    MyAdvertsComponent,
+    AdvertDetailsComponent
   ],
   imports: [
     CommonModule,
